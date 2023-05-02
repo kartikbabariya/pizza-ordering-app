81c4c62e0172573561a4fb120b85f810827db2e7
Kartik Babariya <74000666+kartikbabariya@users.noreply.github.com>
2023-05-02 15:17:37 +0100

    Main Page of pizza app has been done

--- src/App.js
+++ src/App.js
@@ -3,22 +3,24 @@ import './App.css';
 
 import PizzaCheckBox from './Components/PizzaCheckBox';
 import Navbar from './Components/Navbar';
-import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ShowIngredients from './Components/ShowIngredients';
 import DeliveryForm from './Components/DeliveryForm';
+import MainPage from './Components/MainPage';
 
 
 function App() {
   return (
-  <BrowserRouter>
-      <Routes> 
-      <Route path="/" element={<PizzaCheckBox />} />
-        <Route path="/ShowIngredients" element={<ShowIngredients />} />
-        <Route path="/order" element={<DeliveryForm />} />
-      </Routes>
-    </BrowserRouter>
-
-    
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/CustomizePage" element={<PizzaCheckBox />} />
+          <Route path="/ShowIngredients" element={<ShowIngredients />} />
+          <Route path="/order" element={<DeliveryForm />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
---
+++ src/Components/MainPage.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import mainPage from "../Images/mainPage.jpg"
+import { Link } from 'react-router-dom'
+
+const MainPage = () => {
+    return (
+        <>
+            <div className="position-relative">
+
+                <div className='position-absolute'>
+                    <img className='mainPage_image' src={mainPage}  />
+                </div>
+
+                <div className='text-white'>
+                    <div style={{
+                        position: "absolute", left: "50%", marginTop: "15%"
+                    }}>
+                        <div className="d-flex text-center align-item-center" style={{
+                            fontSize: "50px",
+                            color: "#fff"
+                        }}>
+                            Welcome To Our Pizza Customization App
+                        </div>
+                        <div className="d-flex justify-content-center mt-5">
+                            <Link to="/CustomizePage">
+                                <button className="btn btn-primary" type="button" style={{ background: "#ECA502", border: "none", color: "#000", fontSize: "20px", padding: "15px", borderRadius: "15px", fontWeight: "700" }}>Customize Now</button>
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default MainPage
